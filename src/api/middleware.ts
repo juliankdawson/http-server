adb059ffbe74322052af09d7c454f8b470c522ac
Julian Dawson <juliankdawson@gmail.com>
2025-07-25 10:45:55 -0400

    Server requests config

---
+++ src/api/middleware.ts
@@ -0,0 +1,19 @@
+import type { NextFunction, Request, Response } from "express";
+import { config } from "../config.js";
+
+export function middlewareLogResponse(req: Request, res: Response, next: NextFunction): void {
+    res.on("finish", () => {
+        const statusCode = res.statusCode;
+
+        if (statusCode >= 300) {
+            console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${statusCode}`);
+        }
+    });
+
+    next();
+}
+
+export function middlewareMetricsInc(req: Request, res: Response, next: NextFunction) {
+    config.fileServerHits++;
+    next();
+}
